a47341f52c61ab5476e4193f9f012abd13fe3ff3
Jas Aggarwal <1698584+rottitime@users.noreply.github.com>
2022-07-08 12:21:46 +0100

    Typescript

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import Test from './components/Test'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <Test />
         </p>
         <a
           className="App-link"
@@ -19,7 +21,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
